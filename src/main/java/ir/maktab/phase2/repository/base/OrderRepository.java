671d35563169df3ceaaff424f7e4fa78fd995217
Raha <sha.shariat87@gmail.com>
2022-05-11 22:06:20 +0430

    Edit repository

--- src/main/java/ir/maktab/phase2/repository/base/OrderRepository.java
+++ src/main/java/ir/maktab/phase2/repository/base/OrderRepository.java
@@ -1,7 +1,15 @@
 package ir.maktab.phase2.repository.base;
 
 import ir.maktab.phase2.model.Order;
+import ir.maktab.phase2.model.SubSpecialist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order,Integer> {
+
+
+    List<Order> findOrderBySubSpecialistIn(List<SubSpecialist> subSpecialists);
+
+
 }
--- src/main/java/ir/maktab/phase2/repository/base/TechnicianRepository.java
+++ src/main/java/ir/maktab/phase2/repository/base/TechnicianRepository.java
@@ -2,6 +2,19 @@ package ir.maktab.phase2.repository.base;
 
 import ir.maktab.phase2.model.Technician;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface TechnicianRepository extends JpaRepository<Technician,Integer> {
+
+    @Override
+    @Query(value = "select * , 0 As Clazz from person p " +
+            "inner join technician t on t.user_id = p.id " +
+            "inner join service_technician st on st.technician_id=t.user_id " +
+            "inner join sub_specialist sp on sp.id = st.sub_service_id" +
+            " where t.user_id = :id",nativeQuery = true)
+    Optional<Technician> findById(@Param("id") Integer integer);
 }

baf5c09808cc081c7ad6e266c9306eb249d07271
Raha <sha.shariat87@gmail.com>
2022-05-11 09:47:04 +0430

    add offer service test

---
+++ src/test/java/ir/maktab/phase2/service/impl/OfferServiceImplTest.java
@@ -0,0 +1,29 @@
+package ir.maktab.phase2.service.impl;
+
+import ir.maktab.phase2.model.Offer;
+import ir.maktab.phase2.service.OfferService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+
+class OfferServiceImplTest {
+
+
+    @Autowired
+    private OfferServiceImpl offerService;
+    @Test
+    void save(){
+        Offer offer = new Offer();
+        offer.setSuggestPrice(25.6);
+        offer.setDuration("gggg");
+        offerService.save(offer);
+        assertEquals(2,offer.getId());
+    }
+
+}

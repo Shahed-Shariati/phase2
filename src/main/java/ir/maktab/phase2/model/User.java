ceda0f161f639c35216fe7a68378ce01a4738264
Raha <sha.shariat87@gmail.com>
2022-05-11 10:30:34 +0430

    user model modifY

--- src/main/java/ir/maktab/phase2/model/User.java
+++ src/main/java/ir/maktab/phase2/model/User.java
@@ -39,4 +39,52 @@ public class User extends BaseEntity<Integer> {
 
     public User() {
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(LocalDateTime creationTime) {
+        this.creationTime = creationTime;
+    }
 }

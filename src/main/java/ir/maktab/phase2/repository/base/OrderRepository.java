66a0c48f8ec859312aae45c7c375f19de5a0c40c
Raha <sha.shariat87@gmail.com>
2022-05-12 00:09:37 +0430

    add update order repository

--- src/main/java/ir/maktab/phase2/repository/base/OrderRepository.java
+++ src/main/java/ir/maktab/phase2/repository/base/OrderRepository.java
@@ -2,7 +2,11 @@ package ir.maktab.phase2.repository.base;
 
 import ir.maktab.phase2.model.Order;
 import ir.maktab.phase2.model.SubSpecialist;
+import ir.maktab.phase2.model.enumeration.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,5 +15,7 @@ public interface OrderRepository extends JpaRepository<Order,Integer> {
 
     List<Order> findOrderBySubSpecialistIn(List<SubSpecialist> subSpecialists);
 
-
+    @Modifying
+    @Query("update Order o set o.orderStatus = :status where o.id = :id ")
+    Integer upDateOrderStatus(@Param("status") OrderStatus status, @Param("id") Integer id);
 }
